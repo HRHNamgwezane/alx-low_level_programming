53eb919d464649827614d18fe4145b122ebdd738
NaMgwezane <oshunlutho@gmail.com>
2023-07-15 06:42:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints the last line of a quote in standard error.
+ * Main - prints out the ending lines of a quote to the standard error.
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	Return (1)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	Return(1);
 }
