a0bd74462ff271e9d10e1582beebf1189b224bcc
NaMgwezane <oshunlutho@gmail.com>
2023-08-07 05:29:44 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * Prgram that prints its name followed by a new line
+ *
+ * Return: program name
+ */
+int main(int argc, int char* argv[])
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
