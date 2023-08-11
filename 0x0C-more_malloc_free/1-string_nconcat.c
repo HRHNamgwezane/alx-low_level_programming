0357c1d8f9448666dd3bf27659c8409f45de86c8
NaMgwezane <oshunlutho@gmail.com>
2023-08-11 03:17:28 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,12 +27,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
 	if (n >= len2)
+	{
 		n = len2;
+	}
 
 	conctd = malloc(sizeof(char) * (len1 + 1));
 	if (conctd == NULL)
 	{
+		free(conctd);
 		return (NULL);
 	}
 
