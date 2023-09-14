f4f12c512f5d648c3ba30897bb9a4df24a029db4
NaMgwezane <oshunlutho@gmail.com>
2023-09-14 07:02:19 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_mod(int a, int b);
+int op_div(int a, int b);
+
+/**
+ * op_add - reurns sum of two numbers
+ * @a: first input value
+ * @b: second input value
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference of two numbers
+ * @a: first input value
+ * @b: second input value
+ *
+ * Return difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product of two numbers
+ * @a: first input value
+ * @b: second input value
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the quotient of two numbers
+ * @a: first input value
+ * @b: second input value
+ *
+ * Return: quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns remainder of division between two numbers
+ * @a: first input value
+ * @b: second input value
+ *
+ * Return: remainder of division between a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
