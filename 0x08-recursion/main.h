2ebd55dcfe542063c05a6086103bbfb5e505affe
HRHNamgwezane <oshunlutho2gmail.com>
2023-10-25 17:19:14 -0700

    README.md/ main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,8 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int find_sqrt(int n, int min, int max);
-int check_prime(int n, int divisor);
-int check_palindrome(char *s, int start, int end);
 
 #endif
