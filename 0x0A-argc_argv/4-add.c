8c6caa3a094d050c24da73758c7456c97f4034ba
HRHNamgwezane <oshunlutho2gmail.com>
2023-11-01 20:07:38 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_all_digits - check if a string consists of digits only
+ * @str: input string
+ * 
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int is_all_digits(char *str)
+{
+	while (*str)
+	{
+		if (!isdigit(*str))
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (is_all_digits(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
