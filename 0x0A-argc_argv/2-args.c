968000755362ff44ad35860ed7a9c3066926e9da
HRHNamgwezane <oshunlutho2gmail.com>
2023-11-01 19:40:44 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,10 +10,10 @@
  */
 int main(int argc, char *argv[])
 {
-    while (argc--)
-    {
-        printf("%s\n", *(argv++));
-    }
+	while (argc--)
+	{
+		printf("%s\n", *(argv++));
+	}
 
-    return 0;
+	return (0);
 }
