0eed419f827cdc0e3b08e98b82b043c3cd13c2a6
HRHNamgwezane <oshunlutho2gmail.com>
2023-11-09 15:46:57 -0800

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,54 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	int num1 = atoi(argv[1]);
+	char *op = argv[2];
+	int num2 = atoi(argv[3]);
+
+	int result = 0;
+	if (op[1] != '\0' || (op[0] != '+' && op[0] != '-' && op[0] != '*' && op[0] != '/' && op[0] != '%'))
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	if (*op == '+')
+	{
+		result = num1 + num2;
+	}
+	else if (*op == '-')
+	{
+		result = num1 - num2;
+	}
+	else if (*op == '*')
+	{
+		result = num1 * num2;
+	}
+	else if (*op == '/')
+	{
+		result = num1 / num2;
+	}
+	else if (*op == '%')
+	{
+		result = num1 % num2;
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
