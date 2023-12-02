b7b26663c8dc6ab3fac0e4774c88788bb2e2e8e8
HRHNamgwezane <oshunlutho2gmail.com>
2023-12-02 05:15:24 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,17 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * exit_with_error - print error message and exit with specified code
  * @code: exit code
  * @format: format string for dprintf
- * @argv: arguments vector
+ * @filename: file name for the error message
  */
-void exit_with_error(int code, const char *format, char *argv[])
+void exit_with_error(int code, const char *format, const char *filename)
 {
-	dprintf(STDERR_FILENO, format, argv[1]);
+	dprintf(STDERR_FILENO, format, filename);
 	exit(code);
 }
 
@@ -26,30 +28,54 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-		exit_with_error(97, "Usage: cp file_from file_to\n", argv);
+	{
+		exit_with_error(97, "Usage: cp file_from file_to\n", NULL);
+	}
 
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (file_from == -1)
+	{
+		exit_with_error(98, "Error: Can't read from file %s\n", argv[1]);
+	}
+	
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		close(file_from);
+		exit_with_error(99, "Error: Can't write to %s\n", argv[2]);
+	}
 
-	if (file_from == -1 || file_to == -1)
-		exit_with_error(file_from == -1 ? 98 : 99, "Error: Can't %s %s\n", argv);
-
-	do {
+	do
+	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-			exit_with_error(98, "Error: Can't read from file %s\n", argv);
+		{
+			close(file_from);
+			close(file_to);
+			exit_with_error(98, "Error: Can't read from file %s\n", argv[1]);
+		}
+
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
-			exit_with_error(99, "Error: Can't write to %s\n", argv);
-	} while (nchars == 1024);
+		{
+			close(file_from);
+			close(file_to);
+			exit_with_error(99, "Error: Can't write to %s\n", argv[2]);
+		}
+	}
+	while (nchars == 1024);
 
 	err_close = close(file_from);
 	if (err_close == -1)
-		exit_with_error(100, "Error: Can't close fd %d\n", argv);
+	{
+		exit_with_error(100, "Error: Can't close fd %d\n", argv[1]);
+	}
 
 	err_close = close(file_to);
 	if (err_close == -1)
-		exit_with_error(100, "Error: Can't close fd %d\n", argv);
+	{
+		exit_with_error(100, "Error: Can't close fd %d\n", argv[2]);
+	}
 
 	return (0);
 }
