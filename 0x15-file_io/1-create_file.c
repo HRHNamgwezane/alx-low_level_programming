a9eab3607981e18216787415e7010dc6684511ab
HRHNamgwezane <oshunlutho2gmail.com>
2023-12-02 03:48:15 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,10 +11,10 @@
 int create_file(const char *filename, const char *text_content)
 {
 	int fd, nletters, rwr;
-	
+
 	if (!filename)
 		return (-1);
-	
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
